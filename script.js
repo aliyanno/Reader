cf8dd8bc43f42546e9baf5b017c0c951fd5aecdf
aliyanno <aliyamarder@gmail.com>
2014-01-21 21:43:27 -0800

    JSON structure changes after code review

--- script.js
+++ script.js
@@ -1,119 +1,292 @@
+
+
 $(document).ready(function(){
+	window.readerIndex = 0;	
+	var sectionArray = window.story.sections,
+		sectionLength = window.story.sections.length;
 
-	$(window).scroll(function(){ 
-		if ($(window).scrollTop() >= 323) 
-			{$("#navigation").css({
-				'position': 'fixed', 
-				'top': '-200px'
-			}),
-			$(".text-box").css('margin-top', '473px')
-			} 
-		else {$("#navigation").css({
-				'position': 'inherit',
-			})
-			$(".text-box").css('margin-top', '0px')
+
+
+//Set up the Header
+
+	var getTitleTemplate = function() {
+		var titleElement = $("<h1></h1>");
+		titleElement.addClass("title");
+		titleElement.text(window.story.title);
+		return titleElement;
+	};
+
+	var getAuthorTemplate = function() {
+		var authorElement = $("<h4></h4>");
+		authorElement.addClass("author");
+		authorElement.text('by ' + window.story.author);
+		return authorElement
+	};
+
+	var getMainTemplate = function() {
+		var mainElement = $("<h5></h5>");
+		mainElement.addClass("anthology");
+		mainElement.text('in ' + window.story.main);
+		return mainElement
+	};
+
+	var setHeader = function () {
+		$('div#title-container').append(getTitleTemplate())
+			.append(getAuthorTemplate())
+			.append(getMainTemplate());
+	}();
+
+//Set up the Navigation Images
+
+	var getNavTemplate = function () {
+		var imageNav = $("<div></div>");
+		imageNav.addClass("image-nav");
+
+		var getNavImage = function (i) {
+			var sectionImage = sectionArray[i].image,
+				imageTag = $("<img src='" + sectionImage + "'>");
+			imageNav.append(imageTag);
+
+			if (imageTag.index() != window.readerIndex) {
+				imageTag.hide();
+			} else {
+				imageTag.addClass('current');
 			}
-	});
+		};
 
-//Attempt at consolidating JS below
+		for (var i = 0; i < sectionLength; i++) {
+			getNavImage(i);
+		};
 
-/*	var setNewSlide = function(){
-		var $fromSlide = $("div.current");
-		var $imageParent = $fromSlide.parent();
-		var index = $fromSlide.index() + 1;
-		var $toSlide;
-		var $clicked = $(this);
-		
-			if ($clicked.attr("id") == "previous")
-				{var $toSlide = $imageParent.find("#div.image-box:nth-child(" + index - 1 + ")")
-				}
-			else if ($clicked.attr("id") == "next")
-				{var $toSlide = $imageParent.find("#div.image-box:nth-child(" + index + 1 + ")")
-				}
-			else if ($clicked.hasClass(".nav-button"))
-				{var $liParent = $(this).parent();
-				var $buttonIndex = $liParent.index() + 1;
-				var $toSlide = $imageParent.find("#div.image-box:nth-child(" + $buttonIndex + ")")
-				}
+		return imageNav;
 
-		var toIndex = $clicked.index() + 1;
-		$fromSlide.css('z-index', '1').removeClass('current');
-		$(this).css('z-index', '2').addClass('current');
-
-		var $textParent = $(document).find("div.text-box");
-		var $textSection = $textParent.find("div.section:nth-child(" + toIndex + ")");
-		window.scrollTo(0, $textSection.offset().top - 300);
 	};
 
-$(this).setNewSlide; */
+	var setNav = function () {
+		$('div#nav-container').append(getNavTemplate());
+	}();
+
+//Set up the Navigation buttons
 	
-var navNextSlide = function(){
-		var $currentSlide = $("div.current");
-		var $parent = $currentSlide.parent();
-		var $nextSlide;
-		if ($parent.find("div:last-child").hasClass('current')) {
-			$nextSlide = $parent.find("div:first-child");
+	var getNavList = function () {
+		var listNav = $("<div></div>");
+		listNav.addClass("list-nav");
+		var inlineNav = $("<ul></ul>");
+		inlineNav.addClass("inline-nav");
+		listNav.append(inlineNav);
+
+		var getPrevNav = function () {
+			var prevButton = $("<li><a id='previous' href='#'>Previous</a></li>");
+			inlineNav.append(prevButton);
+		};
+
+		var getNextNav = function () {
+			var nextButton = $("<li><a id='next' href='#'>Next</a></li>");
+			inlineNav.append(nextButton);
+		};
+
+		var getNavElem = function () {
+			for (var i = 0; i < sectionLength; i++) {
+				var sectionButton = $("<li></li>");
+				var buttonText = sectionArray[i].nav;
+				var buttonAnchor = $("<a href='#'></a>");
+				buttonAnchor.addClass("nav-button");
+				buttonAnchor.append(buttonText);
+				sectionButton.append(buttonAnchor);
+				inlineNav.append(sectionButton);
+				if (i === window.readerIndex) {
+					buttonAnchor.addClass('active');
+				};
 			}
-		else {
-			$currentSlide
-			$nextSlide = $currentSlide.next();
-		}
+		};
 
-	
-		$currentSlide.css('z-index', '1').removeClass('current'),
-		$nextSlide.css('z-index', '2').addClass('current');
-		var $textParent = $(document).find("div.text-box");
-		var $textSection = $textParent.find("div.section:nth-child(" + index + ")");
-		window.scrollTo(0, $textSection.offset().top - 300);
-		return false;
-	};
-	
-	var navPreviousSlide = function(){
-		var $currentSlide = $("div.current");
-		var $parent = $currentSlide.parent();
-		var $previousSlide;
-		if ($parent.find("div:first-child").hasClass('current')) {
-			$previousSlide = $parent.find("div:last-child");
-		}
-		else {
-			$previousSlide = $currentSlide.prev();
-		}
-	
-		$currentSlide.css('z-index', '1').removeClass('current'),
-		$previousSlide.css('z-index', '2').addClass('current');
-		var $textParent = $(document).find("div.text-box");
-		var $textSection = $textParent.find("div.section:nth-child(" + index + ")");
-		window.scrollTo(0, $textSection.offset().top - 300);
-		return false;
+		getPrevNav();
+		getNavElem();
+		getNextNav();
+		return listNav;
 	};
 
-	var gotoSlide = function (){
-		var $liParent = $(this).parent();
-		var index = $liParent.index() + 1;
-		var $imageParent = $(document).find("div.image-nav");
-		var $matchSlide = $imageParent.find("div:nth-child(" + index + ")"); 
-		$imageParent.find("div.current").css('z-index', '1').removeClass('current'),
-		$matchSlide.css('z-index', '2').addClass('current');
+	var setNavButtons = function () {
+		$('div#nav-container').append(getNavList());
+	}();
 
-		var $textParent = $(document).find("div.text-box");
-		var $textSection = $textParent.find("div.section:nth-child(" + index + ")");
-		window.scrollTo(0, $textSection.offset().top - 300);
+//Set up Content
 
-		return false
+	var getContent = function () {
+		var textBox = $("<div></div>");
+		textBox.addClass('content-box');
+		for (var i = 0; i < sectionLength; i++) {
+			var sectionBox = $("<div></div>");
+			sectionBox.addClass('section-box');
+			sectionBox.attr('id', "" + i + "")
+
+			var fancyHeader = $("<div></div>");
+			fancyHeader.addClass('fancy-header');
+
+			var sectionIcon = sectionArray[i].icon;
+			var fancyImage = $("<img src='" + sectionIcon + "'>");
+
+			var bodyText = $("<div></div>");
+			bodyText.addClass('body-text');
+
+			var sectionContent = (sectionArray[i].text);
+			bodyText.load(sectionContent);
+			
+			fancyHeader.append(fancyImage);
+			sectionBox.append(fancyHeader);
+			sectionBox.append(bodyText);
+			textBox.append(sectionBox);
+		}
+		return textBox;
 	};
 
-	$("#next").click(navNextSlide)
-	$("#previous").click(navPreviousSlide)
-	$("a.nav-button").click(gotoSlide)
+	var setContent = function() {
+		$('div#content').append(getContent());
+	}();
 
+//Change window.readerIndex for Navigation
+	var setReaderIndex = function(i) {
+		window.readerIndex = i;
+	}
 
-	
-	$(document).keyup(function(e) {
-		if (e.which === 39) {navNextSlide();
+//Navigation animation
+
+	var setToImage = function () {
+		var img = $("div.image-nav img"),
+			toImage = $(img.get(window.readerIndex));
+		img.hide();
+		toImage.show();
+		img.removeClass('current');
+		toImage.addClass('current');
+	};
+
+	var scrollToContent = function() {
+		var sectionDiv = $("div.section-box#" + window.readerIndex + "");
+		if (window.readerIndex === 0) {
+			window.scrollTo(0);
+		} else {
+			window.scrollTo(0, sectionDiv.offset().top - 285);
 		}
-		else if (e.which === 37) {navPreviousSlide();
+	};
+
+	var makeButtonActive = function() {
+		var navButtons = $(".nav-button"),
+			navElement = $(navButtons.get(window.readerIndex));
+		navButtons.removeClass('active');
+		navElement.addClass('active');
+	};
+
+	var setActiveOnScroll = function() {
+		setToImage();
+		makeButtonActive();
+	};
+
+	var goToSection = function() {
+		setToImage();
+		scrollToContent();
+		makeButtonActive();
+	};
+
+
+// Set Image index
+
+	var getNextSlide = function (event) {
+		if (window.readerIndex != sectionLength - 1) {
+			setReaderIndex(window.readerIndex + 1);
+			goToSection();
+		}
+		event.preventDefault();
+	};
+
+	var getPrevSlide = function (event) {
+		if (window.readerIndex != 0) {
+			setReaderIndex(window.readerIndex - 1);
+			goToSection();
+		}
+		event.preventDefault();
+	};
+
+	var getAnySlide = function (event) {
+		var buttonText = $(this).text();
+			for (var i = 0; i < sectionLength; i++ ) {
+				if (sectionArray[i].nav === buttonText) {
+					setReaderIndex(i);
+				}
+			}
+		goToSection();
+		event.preventDefault();
+	};
+
+//Change image depending on window position
+	var beginScroll = function() { 
+		var getSectionOffsetValues = function() { // arrays the pixel position of each section in the DOM
+			var result = [];
+			for (var i = 0; i < sectionLength; i++) {
+				var sectionDiv = $("div.section-box#" + i + ""),
+					offsetValue = sectionDiv.offset().top;
+				result.push(offsetValue);
+			};
+			return result;
+		};
+	
+		var getNextIndex = function(windowPosition) { //determines the next section a reader will come to
+			var nextIndex = -1,
+				sectionRelativePosition;
+			for (var i = 1; i < sectionLength; i++)  {
+				sectionRelativePosition = sectionPositionArray[i] - windowPosition;
+				if (sectionRelativePosition >= 0) {
+					nextIndex = i;
+					break;
+				}
+			}
+			if (nextIndex === -1) {
+				nextIndex = sectionLength;
+			}
+			setReaderIndex(nextIndex - 1);
+		};
+
+		var sectionPositionArray = getSectionOffsetValues();
+
+		var setActiveImage = function() { //determines the current section
+			var navSpace = 380; 
+			var windowPosition = $(window).scrollTop() + navSpace;
+			
+			getNextIndex(windowPosition);
+			setActiveOnScroll();
+		};
+
+		setInterval(setActiveImage, 100);
+	};
+
+// Fix Nav at Window Top
+
+	var fixedNav = function(event) {
+		var navBox = $("div#nav-container");
+		var headerArea = 364; //space above nav area that will not be fixed position
+		if ($(window).scrollTop() >= headerArea) {
+			navBox.addClass('fixed-navigation');
+			$("div#content").addClass('below-nav');
+		} else {
+			navBox.removeClass('fixed-navigation');
+			$("div#content").removeClass('below-nav');
+		}
+	};
+	
+// Bind functions 
+	$(window).scroll(fixedNav);
+	setTimeout(beginScroll, 5000);
+	$("#next").click(getNextSlide);
+	$("#previous").click(getPrevSlide);
+	$("a.nav-button").click(getAnySlide);
+
+
+	$(document).keyup(function(event) {
+		if (event.which === 39) {getNextSlide(event);
+		}
+		else if (event.which === 37) {getPrevSlide(event);
 		}	
 	});
-	
-});
 
+	setInterval()
+
+});
---
+++ stories/bohemia/scandal_in_bohemia.js
@@ -0,0 +1,82 @@
+window.story = {
+	'main': 'The Adventures of Sherlock Holmes',
+	'author': 'Arthur Conan Doyle',
+	'title': 'A Scandal In Bohemia',
+	'subtitle': '',
+	'sections': [
+		{
+			'title': 'Part I',
+			'nav': 'I',
+			'icon': 'img/p1.png',
+			'image': 'img/part1.png',
+			'text': 'stories/bohemia/sections/part1.html',
+
+		},
+		{
+			'title': 'Part II',
+			'nav': 'II',
+			'icon': 'img/p2.png',
+			'image': 'img/part2.png',
+			'text': 'stories/bohemia/sections/part2.html',
+
+		},
+		{
+			'title': 'Part III',
+			'nav': 'III',
+			'icon': 'img/p3.png',
+			'image': 'img/part3.png',
+			'text': 'stories/bohemia/sections/part3.html',
+
+		},
+		{
+			'title': 'Part IV',
+			'nav': 'IV',
+			'icon': 'img/p4.png',
+			'image': 'img/part4.png',
+			'text': 'stories/bohemia/sections/part4.html',
+
+		},
+		{
+			'title': 'Part V',
+			'nav': 'V',
+			'icon': 'img/p5.png',
+			'image': 'img/part5.png',
+			'text': 'stories/bohemia/sections/part5.html',
+
+		},
+		{
+			'title': 'Part VI',
+			'nav': 'VI',
+			'icon': 'img/p6.png',
+			'image': 'img/part6.png',
+			'text': 'stories/bohemia/sections/part6.html',
+
+		},
+		{
+			'title': 'Part VII',
+			'nav': 'VII',
+			'icon': 'img/p7.png',
+			'image': 'img/part7.png',
+			'text': 'stories/bohemia/sections/part7.html',
+
+		},
+		{
+			'title': 'Part VIII',
+			'nav': 'VIII',
+			'icon': 'img/p8.png',
+			'image': 'img/part8.png',
+			'text': 'stories/bohemia/sections/part8.html',
+
+		},
+		{
+			'title': 'Part IX',
+			'nav': 'IX',
+			'icon': 'img/p9.png',
+			'image': 'img/part9.png',
+			'text': 'stories/bohemia/sections/part9.html',
+
+		}
+	]
+}
+
+
